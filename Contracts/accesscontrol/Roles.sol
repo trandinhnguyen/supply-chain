6873e00e671beb4b4d479bc0332c179ce6f291cc
Trần Đình Nguyên <nguyentran9122000@gmail.com>
2023-02-05 18:04:41 +0700

    fix Distributor, Retailer roles

--- Contracts/accesscontrol/Roles.sol
+++ Contracts/accesscontrol/Roles.sol
@@ -1,46 +1,36 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
 
-pragma solidity >=0.4.24;
-
-/**
- * @title Roles
- * @dev Library for managing addresses assigned to a Role.
- */
+/// @title Roles
+/// @dev Library for managing addresses assigned to a Role.
 library Roles {
     struct Role {
-        mapping(address => bool) bearer;
+        mapping(address => bool) isRole;
     }
 
-    /**
-     * @dev give an account access to this role
-     */
-    function add(Role storage role, address account) internal {
-        require(account != address(0));
-        require(!has(role, account));
-
-        role.bearer[account] = true;
-    }
-
-    /**
-     * @dev remove an account's access to this role
-     */
-    function remove(Role storage role, address account) internal {
-        require(account != address(0));
-        require(has(role, account));
-
-        role.bearer[account] = false;
-    }
-
-    /**
-     * @dev check if an account has this role
-     * @return bool
-     */
-    function has(Role storage role, address account)
+    /// @dev check if an account has this role
+    function hasRole(Role storage role, address account)
         internal
         view
         returns (bool)
     {
         require(account != address(0));
-        return role.bearer[account];
+        return role.isRole[account];
+    }
+
+    /// @dev give an account access to this role
+    function addRole(Role storage role, address account) internal {
+        require(account != address(0));
+        require(!hasRole(role, account));
+
+        role.isRole[account] = true;
+    }
+
+    /// @dev remove an account's access to this role
+    function removeRole(Role storage role, address account) internal {
+        require(account != address(0));
+        require(hasRole(role, account));
+
+        role.isRole[account] = false;
     }
 }
