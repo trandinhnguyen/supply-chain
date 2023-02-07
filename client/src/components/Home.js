3e44d2607bb8ef2bcf63bc5009f5e38ec55b50e0
Tiến Nam Nguyễn <trieu81200111@gmail.com>
2023-02-07 18:02:28 +0700

    add more logic frontend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -25,76 +25,94 @@ const Home = () => {
   const [farmer, setFarmer] = useState(null);
   const [product, setProduct] = useState(null);
 
-  // const [contract, setContract] = useState(null);
+    // const contractAddress ="0x64a4293c4002923dAE40cDc286Da8CD7cdB9D375"
 
-  const connectWalletHandler = async () => {
-    try {
-      if (!ethereum) return alert("Please install metamask");
+    // const [errorMessage, setErrorMessage] = useState(null);
+	// const [defaultAccount, setDefaultAccount] = useState(null);
+	// const [connButtonText, setConnButtonText] = useState('Connect Wallet');
 
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+	// const [currentOwner, setCurrentOwner] = useState(null);
 
-      setConnButtonText("Wallet Connected");
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.log(error);
-      throw new Error("No ethereum object!");
-    }
+	// const [provider, setProvider] = useState(null);
+	// const [signer, setSigner] = useState(null);
+	// const [contract, setContract] = useState(null);
 
-    // ethereum
-    //   .request({ method: "eth_requestAccounts" })
-    //   .then((result) => {
-    //     accountChangedHandler(result[0]);
-    //     setConnButtonText("Wallet Connected");
-    //   })
-    //   .catch((error) => {
-    //     setErrorMessage(error.message);
-    //   });
-  };
+    // const connectWalletHandler=()=>{
+    //     if(window.ethereum){
+    //         window.ethereum.request({ method: 'eth_requestAccounts'})
+    //         .then(result => {
+	// 			accountChangedHandler(result[0]);
+	// 			setConnButtonText('Wallet Connected');
+	// 		})
+	// 		.catch(error => {
+	// 			setErrorMessage(error.message);
+			
+	// 		});
+    //     }
+    //     else{
+    //         setErrorMessage("Need to install Metamask")
+    //     }
+    // }
 
-  // const accountChangedHandler = (newAccount) => {
-  //   setCurrentAccount(newAccount);
-  // };
+    // const accountChangedHandler = (newAccount) => {
+	// 	setDefaultAccount(newAccount);
+    //     updateEthers();
+	// }
 
-  const addFarmer = async (e) => {
-    try {
-      if (!ethereum) return alert("Please install metamask");
+    // const updateEthers = () => {
+	// 	let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+	// 	setProvider(tempProvider);
 
-      const supplyChainContract = getEthereumContract();
-      e.preventDefault();
-      const supplyChainHash = await supplyChainContract.addFarmer(
-        e.target.farmer.value,
-        e.target.farmerName.value,
-        e.target.farmerRealAddress.value
-      );
+	// 	let tempSigner = tempProvider.getSigner();
+	// 	setSigner(tempSigner);
 
+	// 	let tempContract = new ethers.Contract(contractAddress, SupplyChainContract.abi, tempSigner);
+	// 	setContract(tempContract);	
+	// }
+
+    // const addFarmer = (event) => {
+	// 	event.preventDefault();
+	// 	contract.addFarmerRole(event.target.addFarmer.value);
+	// }
+
+    // const getCurrentOwner = async () => {
+	// 	let val = await contract.owner();
+	// 	setCurrentOwner(val);
+	// }
+
+    return(
+        <div>
+			<div className='content'>
+				<h3>Visit as:</h3>
+				<ul className='menu'>
+					<li>
+						<NavLink to="/admin">Admin</NavLink>
+					</li>
+					<li>
+					<NavLink to="/farmer">Farmer</NavLink>
+					</li>
+				</ul>
+				<hr/>
+			</div>
+            {/* <form onSubmit={addFarmer}>
+				<input id="addFarmer" type="text"/>
+				<button type={"submit"}> Add Farmer </button>
+			</form>
+            <button onClick={getCurrentOwner}> Get Current Contract Owner </button>
+            {currentOwner}
+            {errorMessage} */}
+        </div>
+    )
+}
+
+/*
       await supplyChainHash.wait();
-      // window.location.reload();
     } catch (error) {
       console.log(error);
       throw new Error("No ethereum object!");
     }
-  };
+};
 
-  const addProduct = async (e) => {
-    try {
-      if (!ethereum) return alert("Please install metamask");
-
-      const supplyChainContract = getEthereumContract();
-      e.preventDefault();
-      const supplyChainHash = await supplyChainContract.produceProduct(
-        e.target.name.value,
-        e.target.price.value,
-        e.target.code.value
-      );
-
-      await supplyChainHash.wait();
-    } catch (error) {
-      console.log(error);
-      throw new Error("No ethereum object!");
-    }
-  };
 
   const getCurrentOwner = async () => {
     try {
